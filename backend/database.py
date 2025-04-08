338e3cde40c524d5d02196a62331cfc10f5ff923
Saif <185240419+saifee3@users.noreply.github.com>
2025-04-08 12:47:18 +0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://TODOdb_owner:npg_0lrtXiz3kBOL@ep-shiny-bird-a5kiwx6l-pooler.us-east-2.aws.neon.tech/TODOdb?sslmode=require"
+SQLALCHEMY_DATABASE_URL = "Add Neon DB Connection String Here"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
